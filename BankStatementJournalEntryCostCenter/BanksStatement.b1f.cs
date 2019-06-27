a37bd0dd53d8a5c8bf6b264f5496fc795d5d14c2
nikaku <qurdadzenika@gmail.com>
2019-06-27 11:00:59 +0400

    Fixed Bugs

--- BankStatementJournalEntryCostCenter/BanksStatement.b1f.cs
+++ BankStatementJournalEntryCostCenter/BanksStatement.b1f.cs
@@ -69,31 +69,39 @@ namespace BankStatementJournalEntryCostCenter
 
         private void Button1_PressedAfter(object sboObject, SBOItemEventArg pVal)
         {
+            int successCount = 0;
+            int hasNoEmployeeCount = 0;
+            int hasNoJounralEntryCount = 0;
+            int hasNo3130Count = 0;
+            int errorCount = 0;
+            int totalCount = 0;
             Form activeForm = SAPbouiCOM.Framework.Application.SBO_Application.Forms.ActiveForm;
 
             var matrix = (Matrix)activeForm.Items.Item("10000036").Specific;
             activeForm.Freeze(true);
+
             for (int i = 1; i <= matrix.RowCount; i++)
             {
                 if (((ComboBox)matrix.GetCellSpecific("10000037", i)).Selected == null)
                 {
                     continue;
                 }
+
                 var externalCodeCell = ((ComboBox)matrix.GetCellSpecific("10000037", i)).Selected.Value;
                 var internalCodeCell = ((ComboBox)matrix.GetCellSpecific("10000035", i)).Selected.Value;
-                if (externalCodeCell == "PRL1" || internalCodeCell == "PMD - თანამშრ.")
+
+                if (externalCodeCell == "PRL1" || internalCodeCell == "PMD - თანამშრ." || internalCodeCell == "PBS - 1430")
                 {
+                    totalCount++;
                     int journalEntryTransId;
                     try
                     {
-                        journalEntryTransId = int.Parse(activeForm.DataSources.DBDataSources.Item(0).GetValue("JDTID", 0), CultureInfo.InvariantCulture);
+                        journalEntryTransId = int.Parse(activeForm.DataSources.DBDataSources.Item(0).GetValue("JDTID", i), CultureInfo.InvariantCulture);
                     }
                     catch (Exception e)
                     {
-                        Application.SBO_Application.SetStatusBarMessage("საჟურნალო გატარება არ არსებობს",
-                            BoMessageTime.bmt_Short, true);
-                        activeForm.Freeze(false);
-                        return;
+                        hasNoJounralEntryCount++;
+                        continue;
                     }
                     EmployeesInfo employee = null;
                     var federalTaxIdCell = ((EditText)matrix.GetCellSpecific("1980000087", i)).Value;
@@ -103,44 +111,54 @@ namespace BankStatementJournalEntryCostCenter
                     }
                     catch (Exception)
                     {
-                        Application.SBO_Application.SetStatusBarMessage("თანამშრომელი ვერ მოიძებნა",
-                            BoMessageTime.bmt_Short, true);
-                        return;
+                        hasNoEmployeeCount++;
+                        continue;
                     }
                     var costCenterCode = employee.CostCenterCode;
                     JournalEntries journalEntry = (SAPbobsCOM.JournalEntries)UIConnection.xCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oJournalEntries);
                     journalEntry.GetByKey(journalEntryTransId);
+
                     bool has3130 = false;
+
                     for (int j = 0; j < journalEntry.Lines.Count; j++)
                     {
                         journalEntry.Lines.SetCurrentLine(j);
-                        if (journalEntry.Lines.AccountCode == "3130")
+                        if (journalEntry.Lines.AccountCode == "3130" || journalEntry.Lines.AccountCode == "1430")
                         {
                             journalEntry.Lines.CostingCode2 = costCenterCode;
                             has3130 = true;
                         }
                     }
+
                     var res = journalEntry.Update();
+
                     if (!has3130)
                     {
-                        SAPbouiCOM.Framework.Application.SBO_Application.MessageBox(
-                            "საჟურნალო გატარება არ შეიცავს ანგარიშს : 3130");
-                    }
-                    if (res == 0)
-                    {
-                        SAPbouiCOM.Framework.Application.SBO_Application.MessageBox(
-                            "წარმატებით განახლდა საჟურნალო გატარება");
+                        hasNo3130Count++;
                     }
                     else
                     {
-                        Application.SBO_Application.SetStatusBarMessage("საჟურნალო გატარება ვერ განახლდა :" + UIConnection.xCompany.GetLastErrorDescription(),
-                            BoMessageTime.bmt_Short, true);
+                        if (res == 0)
+                        {
+                            SAPbouiCOM.Framework.Application.SBO_Application.StatusBar.SetSystemMessage(
+                                $"წარმატებით განახლდა საჟურნალო გატარება: {journalEntry.Number}", BoMessageTime.bmt_Short, BoStatusBarMessageType.smt_Success);
+                            successCount++;
+                        }
+                        else
+                        {
+                            Application.SBO_Application.SetStatusBarMessage("საჟურნალო გატარება ვერ განახლდა :" + UIConnection.xCompany.GetLastErrorDescription(),
+                                BoMessageTime.bmt_Short, true);
+                            errorCount++;
+                        }
                     }
+
                 }
 
             }
 
             activeForm.Freeze(false);
+            Application.SBO_Application.MessageBox(
+                $"წარმატებული : {successCount}  {Environment.NewLine} თანამშრომელი არ ყავს : {hasNoEmployeeCount} {Environment.NewLine} 3130 ან 1430 არ აქვს : {hasNo3130Count} {Environment.NewLine} არ აქვს საჟურნალო გატარება : {hasNoJounralEntryCount} {Environment.NewLine} წარუმატებელი : {errorCount} {Environment.NewLine} სულ : {totalCount}");
         }
 
         private void Form_ActivateAfter(SBOItemEventArg pVal)
